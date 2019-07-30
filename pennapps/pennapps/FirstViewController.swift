8e5d8763668dfa1033cccb73dd73c7c3de7eae71
Jothi Ramaswamy <jothiramaswamy@thinksteam4girls.org>
2019-07-30 17:34:15 -0400

    Add files via upload

---
+++ pennapps/pennapps/FirstViewController.swift
@@ -0,0 +1,96 @@
+//
+//  FirstViewController.swift
+//  pennapps
+//
+//  Created by Jothi Ramaswamy on 9/7/18.
+//  Copyright © 2018 Jothi Ramaswamy. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class FirstViewController: UIViewController {
+    
+    var firstName = ""
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var lineChartView: LineChartView!
+
+    func getData() -> [Int] {
+        var final = [0,0,0,0,0,0,0,0,0,0,0,0]
+        let events = UserDefaults.standard.array(forKey: "added")
+        
+        if events == nil {
+            return final
+        }
+        for e in events! {
+            var event = e as! Int
+            if event < 0 {
+                event += 12
+            }
+            if (11-event > -2){
+                final[event-1] += 1
+            }
+        }
+        return final;
+    }
+    
+    func setChartValues(_ count: Int = 6) {
+//        let values = (0..<count).map { (i) -> ChartDataEntry in
+//            let val = Double(arc4random_uniform(UInt32(count)) + 3)
+//            return ChartDataEntry(x: Double(i), y: val)
+//        }
+        let a = [1,2,3,4,5,6,7,8,9,10,11,12]
+        let b = getData()
+        
+        var dataEntries : [ChartDataEntry] = []
+        
+        for index in 0..<b.count{
+            let dataEntry = ChartDataEntry(x: Double(a[index]), y: Double(b[index]))
+            dataEntries.append(dataEntry)
+        }
+        
+        
+        let set1 = LineChartDataSet(values: dataEntries, label: "null")
+        let data = LineChartData(dataSet: set1)
+        
+        let color = NSUIColor(red: 0.122*4, green: 0.203*4, blue: 0.185*4, alpha: 1)
+        
+        set1.colors = [NSUIColor.lightGray]
+        set1.circleColors = [color]
+        self.lineChartView.data = data
+        lineChartView.xAxis.labelPosition = .bottom
+        lineChartView.dragEnabled = true
+        lineChartView.setVisibleXRangeMaximum(5)
+        self.lineChartView.legend.enabled = false
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let name = UserDefaults.standard.string(forKey: "name") else {
+            firstName = "there"
+            return
+        }
+        firstName = name.components(separatedBy: ",")[0]
+        titleLabel.text = "Hello, \(firstName)!"
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        setChartValues()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print(getData())
+        setChartValues()
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
